e566346dcfd81f8c62584364d0da244f04a417cb
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-04-16 17:10:46 +0000

    imported datetime, emailvalidator, and validate_email, and added a contacSubmission class in the models.py file

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy.orm import validates
+from email_validator import validate_email, EmailNotValidError
 
 db = SQLAlchemy()
 
@@ -17,3 +20,13 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class ContactSubmission(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    subject = db.Column(db.String(200), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<ContactSubmission {self.name}>'
