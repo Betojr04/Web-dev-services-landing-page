4dbb43b4dffaadc32e30dcd9539e979896b007a7
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-04-18 18:34:39 +0000

    erased the navbar that came prebuilt and started building my own

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,26 @@
 import React from "react";
+import "../../styles/navbar.css";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <div className="navbar">
+      <div className="navbar-container">
+        <nav className="navbar-menu">
+          <Link to="/home" className="navbar-link">
+            Home
+          </Link>
+          <Link to="/home" className="navbar-link">
+            About
+          </Link>
+          <Link to="/home" className="navbar-link">
+            Portfolio
+          </Link>
+          <Link to="/home" className="navbar-link">
+            Contact
+          </Link>
+        </nav>
+      </div>
+    </div>
+  );
 };
