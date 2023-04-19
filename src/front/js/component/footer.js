135f4ca108af4c476b37058148e8680abac48d58
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-04-19 21:41:18 +0000

    made the icons a tags so that they vould link to their respective pagess

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "../../styles/footer.css";
 
 export const Footer = () => (
   <footer className="footer mt-auto py-3 text-center">
     <div className="icon-container">
-      <i className="fa-brands fa-github"></i>
-      <i className="fa-brands fa-linkedin"></i>
-      <i className="fa-brands fa-youtube"></i>
-      <i className="fa-brands fa-instagram"></i>
+      <a href="https://github.com/Betojr04">
+        <i className="fa-brands fa-github"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/albertovaltierrajr">
+        <i className="fa-brands fa-linkedin"></i>
+      </a>
+      <a>
+        <i className="fa-brands fa-youtube"></i>
+      </a>
+      <a>
+        <i className="fa-brands fa-instagram"></i>
+      </a>
     </div>
   </footer>
 );
