5b3609168d51d4900e00713954f8e3d93da2fc9f
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-05-01 12:45:11 +0000

    finishing touches on about me section in the contect section

--- src/front/js/pages/AboutMe.js
+++ src/front/js/pages/AboutMe.js
@@ -5,21 +5,20 @@ const AboutMe = () => {
     <section id="about">
       <h2>About Me</h2>
       <img src="your-photo.jpg" alt="Your Name" />
-      <h3>Your Name</h3>
+      <h3>Alberto Valtierra</h3>
       <p class="subtitle">Full Stack Software Developer | YourLocation</p>
       <p>
-        I'm [Your Name], a full stack developer skilled in [core skills, e.g.,
-        JavaScript, React, Node.js, Python, etc.]. Having completed an intensive
-        bootcamp, I'm enthusiastic about creating efficient, user-friendly web
-        applications and continuously learning to stay up-to-date with industry
-        trends.
+        I'm Alberto Valtierra Jr, a full stack developer skilled in [JavaScript,
+        React, Node.js, Python, SQL, PostgreSQL, Flask, Restful APIs,]. Having
+        completed an intensive bootcamp, I'm enthusiastic about creating
+        efficient, user-friendly web applications and continuously learning to
+        stay up-to-date with industry trends.
       </p>
       <p>
-        Outside of coding, I enjoy [mention one or two personal hobbies or
-        interests, e.g., hiking, photography, etc.], which keeps me inspired and
-        balanced.
+        Outside of coding, I enjoy hiking & running, exercising, and
+        photography/videography, which keeps me inspired and balanced.
       </p>
-      <a href="your-contact-link" class="cta">
+      <a href="/contactme" class="cta">
         Interested in working together? Get in touch
       </a>
     </section>
