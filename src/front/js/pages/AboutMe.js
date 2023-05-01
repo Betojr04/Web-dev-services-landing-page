04cc3890261944e62c64ee04be0d205c3f65c33a
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-05-01 12:39:06 +0000

    edited the about me page to show a proper description of me and proper visual

--- src/front/js/pages/AboutMe.js
+++ src/front/js/pages/AboutMe.js
@@ -2,15 +2,27 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <div className="container">
-      <div className="text-center">
-        <main>
-          <h1>About Me</h1>
-          <p>Software Developer and Entreprenuer</p>
-          <p>I am a Software Developer</p>
-        </main>
-      </div>
-    </div>
+    <section id="about">
+      <h2>About Me</h2>
+      <img src="your-photo.jpg" alt="Your Name" />
+      <h3>Your Name</h3>
+      <p class="subtitle">Full Stack Software Developer | YourLocation</p>
+      <p>
+        I'm [Your Name], a full stack developer skilled in [core skills, e.g.,
+        JavaScript, React, Node.js, Python, etc.]. Having completed an intensive
+        bootcamp, I'm enthusiastic about creating efficient, user-friendly web
+        applications and continuously learning to stay up-to-date with industry
+        trends.
+      </p>
+      <p>
+        Outside of coding, I enjoy [mention one or two personal hobbies or
+        interests, e.g., hiking, photography, etc.], which keeps me inspired and
+        balanced.
+      </p>
+      <a href="your-contact-link" class="cta">
+        Interested in working together? Get in touch
+      </a>
+    </section>
   );
 };
 
