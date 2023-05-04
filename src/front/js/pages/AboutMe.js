0a860a1827f7282165dc9de250cdf7ab56d10f1d
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-05-04 03:30:09 +0000

    fixed the navbar so it is part of normal flow of the document so it doesnt overlap all the pages

--- src/front/js/pages/AboutMe.js
+++ src/front/js/pages/AboutMe.js
@@ -2,26 +2,28 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <section id="about">
-      <h2>About Me</h2>
-      <img src="your-photo.jpg" alt="Your Name" />
-      <h3>Alberto Valtierra</h3>
-      <p class="subtitle">Full Stack Software Developer | YourLocation</p>
-      <p>
-        I'm Alberto Valtierra Jr, a full stack developer skilled in [JavaScript,
-        React, Node.js, Python, SQL, PostgreSQL, Flask, Restful APIs,]. Having
-        completed an intensive bootcamp, I'm enthusiastic about creating
-        efficient, user-friendly web applications and continuously learning to
-        stay up-to-date with industry trends.
-      </p>
-      <p>
-        Outside of coding, I enjoy hiking & running, exercising, and
-        photography/videography, which keeps me inspired and balanced.
-      </p>
-      <a href="/contactme" class="cta">
-        Interested in working together? Get in touch
-      </a>
-    </section>
+    <div>
+      <section className="container" id="about">
+        <h2>About Me</h2>
+        <img src="your-photo.jpg" alt="Your Name" />
+        <h3>Alberto Valtierra</h3>
+        <p class="subtitle">Full Stack Software Developer | YourLocation</p>
+        <p>
+          I'm Alberto Valtierra Jr, a full stack developer skilled in
+          [JavaScript, React, Node.js, Python, SQL, PostgreSQL, Flask, Restful
+          APIs,]. Having completed an intensive bootcamp, I'm enthusiastic about
+          creating efficient, user-friendly web applications and continuously
+          learning to stay up-to-date with industry trends.
+        </p>
+        <p>
+          Outside of coding, I enjoy hiking & running, exercising, and
+          photography/videography, which keeps me inspired and balanced.
+        </p>
+        <a href="/contactme" class="cta">
+          Interested in working together? Get in touch
+        </a>
+      </section>
+    </div>
   );
 };
 
