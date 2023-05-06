89455eac433b24534a0b57f01a89c18bf6265fcc
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-05-06 22:55:12 +0000

    edited the content of the about me section and styled the image to the left of the content

--- src/front/js/pages/AboutMe.js
+++ src/front/js/pages/AboutMe.js
@@ -10,20 +10,23 @@ const AboutMe = () => {
           <h2>About Me</h2>
           <img src="beto.jpeg" alt="Your Name" />
           <h3>Alberto Valtierra</h3>
-          <p class="subtitle">Full Stack Software Developer | YourLocation</p>
+          <p class="subtitle">
+            Full Stack Software Developer | Phoenix, Arizona
+          </p>
           <p>
             I'm Alberto Valtierra Jr, a full stack developer skilled in
             [JavaScript, React, Node.js, Python, SQL, PostgreSQL, Flask, Restful
-            APIs,]. Having completed an intensive bootcamp, I'm enthusiastic
-            about creating efficient, user-friendly web applications and
-            continuously learning to stay up-to-date with industry trends.
+            APIs, Scrum]. Having completed an intensive bootcamp, I'm
+            enthusiastic about creating efficient, user-friendly web
+            applications and continuously learning to stay up-to-date with
+            industry trends.
           </p>
           <p>
             Outside of coding, I enjoy hiking & running, exercising, and
             photography/videography, which keeps me inspired and balanced.
           </p>
           <a href="/contactme" class="cta">
-            Interested in working together? Get in touch
+            Interested in working together? Get in touch here
           </a>
         </section>
       </div>
