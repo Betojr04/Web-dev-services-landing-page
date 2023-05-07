183d9adcf1fb8abc712268f9b4bac1a6c46f621c
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-05-07 18:32:07 +0000

    added projectData component to hold the project data separately and call it using props

---
+++ src/front/js/component/projectData.js
@@ -0,0 +1,16 @@
+const projectData = [
+  {
+    title: "Project 1",
+    description: "This is a description for project 1.",
+    technologies: ["React", "Flask", "MongoDB"],
+    repoLink: "https://github.com/your-username/project-1",
+  },
+  {
+    title: "Project 2",
+    description: "This is a description for project 2.",
+    technologies: ["Node.js", "Express", "PostgreSQL"],
+    repoLink: "https://github.com/your-username/project-2",
+  },
+];
+
+export default projectData;
