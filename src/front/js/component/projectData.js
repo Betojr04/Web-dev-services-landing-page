95258aa45149eb5acce853c9f219ec15fc2e6ebc
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-06-10 23:08:36 +0000

    changed some of the content on the projectData file

--- src/front/js/component/projectData.js
+++ src/front/js/component/projectData.js
@@ -11,13 +11,14 @@ const projectData = [
       "Python",
       "SQL",
       "PostgreSql",
+      "Node.js",
     ],
     repoLink: "https://github.com/Betojr04/NoStrings.git",
   },
   {
-    title: "MoneyMate",
+    title: "Personal Finance Managment",
     description:
-      "Developing a secure and feature-rich personal finance web app, incorporating custom and Yodlee APIs for bank account integration, user data encryption, privacy, and authentication, alongside responsive design, state management, rigorous testing, CI/CD, localization, PWA capabilities, performance optimization, accessibility, and thorough documentation. Built using React, Bootstrap, Python, Flask, and SQL-Alchemy to provide an exceptional user experience.",
+      "Developing a secure and feature-rich personal finance web app, incorporating custom and Plaid APIs for bank account integration, user data encryption, privacy, and authentication, alongside responsive design, state management, rigorous testing, CI/CD, localization, PWA capabilities, performance optimization, accessibility, and thorough documentation. Built using React, Bootstrap, Python, Flask, and SQL-Alchemy to provide an exceptional user experience.",
     technologies: [
       "React",
       "Javascript",
@@ -26,6 +27,7 @@ const projectData = [
       "Flask",
       "SQL-Alchemy",
       "Rest API's",
+      "Node.js",
     ],
     repoLink: "https://github.com/Betojr04/Money-Mate.git",
   },
