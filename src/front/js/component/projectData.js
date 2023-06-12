32ef3e9ddb63fc6f97091293d1cc8813f8094d9a
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-06-12 21:35:16 +0000

    edited the projects info to reflect the projects I am working on

--- src/front/js/component/projectData.js
+++ src/front/js/component/projectData.js
@@ -29,10 +29,26 @@ const projectData = [
       "Rest API's",
       "Node.js",
     ],
-    repoLink: "https://github.com/Betojr04/Money-Mate.git",
+    repoLink: "https://github.com/Betojr04/Personal-Finance-Management.git",
   },
   {
-    title: "Personal Finance Managment",
+    title: "Shift Bidding Program",
+    description:
+      "Leveraging React and Flask, this project revolutionizes shift allocation by providing employees with the ability to bid on preferred shifts based on seniority. The front-end is built using React, offering a modern and intuitive user interface. The back-end is powered by Flask, facilitating seamless communication between the client and server. This ongoing project showcases my commitment to continuous learning and applying new technologies to solve real-world challenges.",
+    technologies: [
+      "React",
+      "Javascript",
+      "PostgreSQL",
+      "Python",
+      "Flask",
+      "SQL-Alchemy",
+      "Rest API's",
+      "Node.js",
+    ],
+    repoLink: "https://github.com/Betojr04/CBRO-SHIFT-BIDDING.git",
+  },
+  {
+    title: " Historical Stock Data",
     description:
       "Developing a secure and feature-rich personal finance web app, incorporating custom and Plaid APIs for bank account integration, user data encryption, privacy, and authentication, alongside responsive design, state management, rigorous testing, CI/CD, localization, PWA capabilities, performance optimization, accessibility, and thorough documentation. Built using React, Bootstrap, Python, Flask, and SQL-Alchemy to provide an exceptional user experience.",
     technologies: [
@@ -45,23 +61,7 @@ const projectData = [
       "Rest API's",
       "Node.js",
     ],
-    repoLink: "https://github.com/Betojr04/Money-Mate.git",
-  },
-  {
-    title: "Personal Finance Managment",
-    description:
-      "Developing a secure and feature-rich personal finance web app, incorporating custom and Plaid APIs for bank account integration, user data encryption, privacy, and authentication, alongside responsive design, state management, rigorous testing, CI/CD, localization, PWA capabilities, performance optimization, accessibility, and thorough documentation. Built using React, Bootstrap, Python, Flask, and SQL-Alchemy to provide an exceptional user experience.",
-    technologies: [
-      "React",
-      "Javascript",
-      "PostgreSQL",
-      "Python",
-      "Flask",
-      "SQL-Alchemy",
-      "Rest API's",
-      "Node.js",
-    ],
-    repoLink: "https://github.com/Betojr04/Money-Mate.git",
+    repoLink: "https://github.com/Betojr04/Historical-Stock-Data.git",
   },
 ];
 
